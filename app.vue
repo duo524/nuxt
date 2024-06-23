2d8bbdbb55a7707a11ffc2cec0d194de60710476
renjinduo <r1575976602@163.com>
2024-06-23 11:43:05 +0800

    feat: 修改启动命令

--- app.vue
+++ app.vue
@@ -4,3 +4,21 @@
     <NuxtWelcome />
   </div>
 </template>
+
+
+<script setup lang="ts">
+const email = useState(() => '')
+const onLogin = () => {
+  $fetch('/api/login', {
+    method: 'post',
+    body: {
+      email: 'r1575976602@163.com',
+    },
+  }).then((user) => {
+    console.log(user)
+  }).catch((err) => {
+    console.log(err)
+  })
+}
+onLogin()
+</script>
